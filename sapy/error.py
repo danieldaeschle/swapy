82a92f49394208e22d4477f5cc50e34d9b7ce10d
danieldaeschle <daniel.daeschle@gmail.com>
2018-01-18 12:36:35 +0100

    added setup

--- sapy/error.py
+++
@@ -1,7 +0,0 @@
-from flask import jsonify
-
-
-def json_catch():
-    def handle(error, code):
-        return jsonify({'message': str(error), 'status_code': code})
-    return handle
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='sapy',
+    version='0.1',
+    description='Web micro framework built for APIs',
+    author='Daniel Däschle',
+    author_email='daniel.daeschle@gmail.com',
+    url='https://github.com/danieldaeschle/sapy',
+    packages=['werkzeug']
+)
