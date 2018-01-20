06891e9e55679126f4e65a271ee8e693ad027a4e
Daniel <daniel.daeschle@gmail.com>
2018-01-20 22:16:32 +0100

    changed setup description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='swapy',
     version='0.1',
-    description='Web micro framework built for APIs',
+    description='More easier and modular web development',
     author='Daniel Däschle',
     author_email='daniel.daeschle@gmail.com',
     url='https://github.com/danieldaeschle/swapy',
