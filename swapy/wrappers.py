393cba4b584c7568cb2b95786baaa6e6001740d5
danieldaeschle <daniel.daeschle@gmail.com>
2018-02-08 15:40:34 +0100

    extended docs and some fixes

--- swapy/wrappers.py
+++ swapy/wrappers.py
@@ -1,5 +1,5 @@
 from werkzeug.wrappers import BaseRequest
-from werkzeug.contrib.securecookie import SecureCookie as SecureCookie
+from werkzeug.contrib.securecookie import SecureCookie
 import json
 
 
@@ -7,11 +7,10 @@ def response_from(args):
     """
     Casts a result from a route function into a response object
 
-    :param args: object
+    :param args: tuple | list | Response
     :return: Response
     """
-    print()
-    if isinstance(args, tuple):
+    if isinstance(args, (tuple, list)):
         if len(args) == 1 and isinstance(args[0], Response):
             return args[0]
         return Response(*args)
@@ -25,6 +24,9 @@ class Request(BaseRequest):
     """
     Request class which inherits from werkzeug's request class
     It adds the json function
+
+    Example:
+        request = Request('content', 200, {'my_header': 'value'})
     """
     session = None
     _secure_cookie = None
@@ -46,9 +48,19 @@ class Request(BaseRequest):
 
     @property
     def secure_cookie(self):
+        """
+        Returns the secure cookie object from werkzeug
+
+        :return: SecureCookie
+        """
         return self.get_secure_cookie()
 
     def get_secure_cookie(self):
+        """
+        Returns the secure cookie object from werkzeug
+
+        :return: SecureCookie
+        """
         secret_key = self.state.environment.get('secret_key')
         if not secret_key:
             raise Exception('\'secret_key\' value must be set in environment')
@@ -72,6 +84,11 @@ class Response:
 
     @property
     def cookies(self):
+        """
+        Setter and getter property for cookies
+
+        :return: dict
+        """
         return self.get_cookies()
 
     @cookies.setter
@@ -79,8 +96,18 @@ class Response:
         self.set_cookies(data)
 
     def set_cookies(self, data):
+        """
+        Adds cookies to the current
+
+        :param data: dict
+        """
         for key in data.keys():
             self._cookies[key] = data[key]
 
     def get_cookies(self):
+        """
+        Returns all set cookies
+
+        :return: dict
+        """
         return self._cookies
