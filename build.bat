515c2ab0bf9bb5ed4fd7ee62a3dcd114e0c5d91a
danieldaeschle <daniel.daeschle@gmail.com>
2018-02-08 22:28:02 +0100

    updated build file

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
-echo Cleaning dist directory...
+echo Cleaning directories...
 del -R dist
+del -R swapy.egg-info
+del MANIFEST
+del -R .cache
 echo Building packages...
 python setup.py sdist
 python setup.py wheel
